68c989acfd0b846b0a7f735666e7934c9a1f5962
Josep Plana <josep.plana.riu@upc.edu>
2023-11-20 15:22:55 +0100

    PIT simulation process started

---
+++ parallel_in_time/channel_flow/specific_mods/flowPost.cpp
@@ -0,0 +1,451 @@
+#include <algorithm>
+#include <cfloat>
+#include <iomanip>
+#include "tf2/DataIO.h"
+#include "tf2/IO.h"
+#include "tf2/Simulation.h"
+#include "tf2/Opers.h"
+#include "tf2/ll_Opers.h"
+
+static const double snap = 1e-8;
+
+struct fLess
+{
+    constexpr bool operator()(const double &lhs, const double &rhs) const
+    {
+        return (lhs < rhs && fabs(lhs-rhs) > snap);
+    }
+};
+
+uint64_t bsearchVectorE(const std::vector<double> &vec, double target, double eps)
+{
+    // Given a vector 'vec' such that vec[i]>vec[i-1], find the largest i such
+    // that vec[i]<=target using a binary search.
+    uint64_t li = 0, ui = vec.size()-1;
+    while ((ui-li)>1)
+    {
+        uint64_t mi = (li+ui)/2;
+        if (fabs(vec[mi]-target) < eps)
+        {
+            return mi;
+        }
+        else if (vec[mi]>target)
+        {
+            ui = mi;
+        }
+        else
+        {
+            li = mi;
+        }
+    }
+    return vec[ui] <= target? ui : li;
+}
+
+std::vector<double> allReduce(const std::vector<double> &in, MPI_Op op, MPI_Comm w = MPI_COMM_WORLD)
+{
+    std::vector<double> result(in.size());
+    int32_t sz = TO_S32(in.size());
+    tf2::checkr(MPI_Allreduce(in.data(), result.data(), sz, MPI_DOUBLE, op, w),
+                "allreduce");
+    return result;
+}
+
+extern "C"
+{
+
+std::vector<double> calcFacesYPos(tf2::Simulation &sim)
+{
+    const auto &mesh  = tf2::getSMesh(sim);
+    std::set<double, fLess> facesY;
+    uint32_t NfY = TO_U32(sim.IOParamI["NY"]+1);
+    const auto faces = tf2::getNumFaces(mesh);
+
+    for (int f = 0; f < faces; ++f)
+    {
+        const auto &F = tf2::getFaceIJKFromId(mesh,f);
+	double fy = tf2::calcFaceCentroid(mesh,F)[1];
+        double nvy = tf2::calcFaceNormal(mesh,F)[1];
+        if (fabs(nvy) > 0.99)
+        {
+            facesY.insert(fy);
+            if (facesY.size() == NfY)
+            {
+                break;
+            }
+        }
+    }
+    TF_uAssert(facesY.size() <= NfY);
+
+    std::vector<double> localFY;
+    localFY.reserve(facesY.size());
+    for (auto it : facesY)
+    {
+        localFY.push_back(it);
+    }
+    auto tmp = tf2::allGatherV(localFY);
+    std::sort(tmp.begin(), tmp.end());
+
+    std::vector<double> finalFY;
+    finalFY.reserve(NfY);
+    auto p = tmp.begin();
+    finalFY.push_back(*p);
+    p++;
+    for (auto pe = tmp.end(); p != pe; p++)
+    {
+        if (fabs(*p-finalFY.back()) > snap)
+        {
+            finalFY.push_back(*p);
+        }
+    }
+
+    TF_uAssert(finalFY.size() == NfY, "my size is "<<finalFY.size());
+    return finalFY;
+}
+
+/*void initAvgField(tf2::Simulation &sim)
+{
+    auto &fab = tf2::getOrCreateField(sim, sim.IOParamI["nSims"], "avgBottom", "Faces");
+    const auto &mesh = tf2::getSMesh(sim);
+    std::vector<double> buf_fab(tf2::getNumFaces(mesh));
+    const auto bound_faces = tf2::getNumBFaces(mesh);
+    for (int bf = 0; bf<bound_faces; ++bf)
+    {
+        const auto &BF = tf2::getBFaceIJKFromId(mesh,bf);
+        if (tf2::getBoCoId(mesh,BF) == tf2::SMesh::Bottom)
+        {
+            buf_fab[bf] = tf2::calcFaceArea(mesh,BF);
+        }
+    }
+    tf2::oper_setData(fab, buf_fab.data());
+    tf2::Field one(sim, sim.IOParamI["nSims"], "Faces");
+    tf2::oper_setConst(one, 1.0);
+    sim.IOParamD["bottomArea"] = tf2::oper_dot(one, fab)[0];
+}*/
+
+
+void initAvgField(tf2::Simulation &sim)
+{
+    auto &fab = tf2::getOrCreateField(sim, 1, "avgBottom", "Faces");
+    const auto &mesh = tf2::getSMesh(sim);
+    std::vector<double> buf_fab(tf2::getNumEntries(fab));
+    const auto faces = tf2::getNumFaces(mesh);
+    for (uint32_t it = 0; it < faces; it++)
+    {
+        const auto &F = tf2::getFaceIJKFromId(mesh, it);
+        if (tf2::getBoCoId(mesh, F) == 3)
+        {
+            const auto &areas = calcCellFacesArea(mesh, F);
+            buf_fab[it] = areas[1];
+            // areas[1] because boco 3 is an 'y' face; for 'x' faces it would
+            // have been areas[0], for 'z' faces it would have been areas[2].
+        }
+    }
+    tf2::oper_setData(fab, buf_fab.data());
+    tf2::Field one(sim, 1, "Faces");
+    tf2::oper_setConst(one, 1.0);
+    sim.IOParamD["bottomArea"] = tf2::oper_dot(one, fab)[0];
+}
+
+void load(tf2::Simulation &sim)
+{
+    const auto &name = tf2::substituteParams(sim, sim.IOParamS["load"]);
+    tf2::loadFields(sim, name);
+}
+
+void averageXZ(tf2::Simulation &sim)
+{
+    const auto &facesYPos = calcFacesYPos(sim);
+    auto NY = facesYPos.size()-1;
+    std::vector<double> avguL(NY);
+    std::vector<double> avgwL(NY);
+    std::vector<double> avgpL(NY);
+    std::vector<double> volumesL(NY);
+    std::vector<double> yL(NY, -DBL_MAX);
+    std::vector<double> avgdudyL(NY+1);
+    std::vector<double> avgdwdyL(NY+1);
+    std::vector<double> areasL(NY+1);
+    std::vector<double> yfL(NY+1, -DBL_MAX);
+
+    auto &GY = tf2::getMatrix(sim, "GradY_NF");
+    auto &ux = tf2::getField(sim, "ens_avg(ux)");
+    auto &uz = tf2::getField(sim, "ens_avg(uz)");
+    auto &dudy = tf2::getOrCreateField(sim, 1, "dudy", "Faces");
+    tf2::Field dwdy(sim, 1, "Faces");
+
+    tf2::oper_prod(GY, ux, dudy);
+    tf2::oper_prod(GY, uz, dwdy);
+    auto &fab = tf2::getField(sim, "avgBottom");
+    double bottomArea = sim.IOParamD["bottomArea"];
+    double sdudy = fabs(tf2::oper_dot(fab, dudy)[0])/bottomArea;
+    tf2::info("Calculated Re_tau is %.5e\n",sdudy);
+
+    const auto &mesh = tf2::getSMesh(sim);
+    std::vector<double> bufferu(tf2::getNumNodes(mesh));
+    std::vector<double> bufferw(tf2::getNumNodes(mesh));
+    std::vector<double> bufferp(tf2::getNumNodes(mesh));
+    tf2::oper_getData(ux, bufferu.data());
+    tf2::oper_getData(uz, bufferw.data());
+    auto cells = tf2::getNumCells(mesh);
+    for (uint32_t c = 0; c < cells; c++)
+    {
+        const auto &C = tf2::getCellIJKFromId(mesh,c);
+	double cy = tf2::calcCellCentroid(mesh,C)[1];
+        double vol = tf2::calcCellVolume(mesh,C);
+        uint64_t idp = bsearchVectorE(facesYPos, cy, snap);
+        avguL[idp] += vol*bufferu[c];
+        avgwL[idp] += vol*bufferw[c];
+        volumesL[idp] += vol;
+        yL[idp] = cy;
+    }
+
+    const auto &outP = sim.IOParamS["outPrefix"];
+    const auto &avgu = allReduce(avguL, MPI_SUM);
+    const auto &avgw = allReduce(avgwL, MPI_SUM);
+    const auto &volumes = allReduce(volumesL, MPI_SUM);
+    const auto &y = allReduce(yL, MPI_MAX);
+    const auto Re = sim.IOParamD["Re_tau"];
+    const auto fct = sim.IOParamD["RKfct"];
+    const auto method = sim.IOParamS["RKmethod"];
+    {
+    std::ofstream out("results/profile_Re"+std::to_string(Re)+"_fct"+std::to_string(fct)+"_cells_"+method+".dat");
+    TF_uAssert(out.is_open(), "could not open filename for output");
+    out<<"#y y+ uavg wavg pavg\n";
+    out<<std::scientific<<std::setprecision(8);
+    if (tf2::mpiRank() == 0)
+    {
+        for (uint32_t k = 0; k < NY; k++)
+        {
+            out<<y[k]
+               <<" "<<y[k]*sdudy
+               <<" "<<avgu[k]/volumes[k]
+               <<" "<<avgw[k]/volumes[k]
+               <<"\n";
+        }
+    }
+    out.close();
+    }
+
+    std::vector<double> bufferdudy(tf2::getNumFaces(mesh));
+    std::vector<double> bufferdwdy(tf2::getNumFaces(mesh));
+    tf2::oper_getData(dudy, bufferdudy.data());
+    tf2::oper_getData(dwdy, bufferdwdy.data());
+    auto faces = tf2::getNumFaces(mesh);
+    for (uint32_t f=0; f<faces; ++f)
+    {
+        const auto &F = tf2::getFaceIJKFromId(mesh,f);
+        auto nvy = tf2::calcFaceNormal(mesh,F)[1];
+        if (fabs(nvy) > 0.99)
+        {
+            double cy = tf2::calcFaceCentroid(mesh,F)[1];
+	    const auto &areas = calcCellFacesArea(mesh,F);
+            double area = fabs(areas*tf2::calcFaceNormal(mesh,F));
+            uint64_t idp = bsearchVectorE(facesYPos, cy, snap);
+            avgdudyL[idp] += area*bufferdudy[f];
+            avgdwdyL[idp] += area*bufferdwdy[f];
+            areasL[idp] += area;
+            yfL[idp] = cy;
+        }
+    }
+
+    const auto &avgdudy = allReduce(avgdudyL, MPI_SUM);
+    const auto &avgdwdy = allReduce(avgdwdyL, MPI_SUM);
+    const auto &areas = allReduce(areasL, MPI_SUM);
+    const auto &yf = allReduce(yfL, MPI_MAX);
+    {
+    std::ofstream out("results/profile_Re"+std::to_string(Re)+"_fct"+std::to_string(fct)+"_faces_"+method+".dat");
+    TF_uAssert(out.is_open(), "could not open filename for output");
+    out<<"#y y+ duavg/dy dwavg/dy\n";
+    out<<std::scientific<<std::setprecision(8);
+    if (tf2::mpiRank() == 0)
+    {
+        for (uint32_t k = 0; k < NY+1; k++)
+        {
+            out<<yf[k]
+               <<" "<<yf[k]*sdudy
+               <<" "<<avgdudy[k]/areas[k]
+               <<" "<<avgdwdy[k]/areas[k]
+               <<"\n";
+        }
+    }
+    out.close();
+    }
+}
+
+void ReynoldsStresses(tf2::Simulation &sim)
+{
+    auto &avg_u = tf2::getField(sim, "ens_avg(ux)");
+    auto &avg_v = tf2::getField(sim, "ens_avg(uy)");
+    auto &avg_w = tf2::getField(sim, "ens_avg(uz)");
+    auto &avg_uu = tf2::getField(sim, "ens_avg(ux*ux)");
+    auto &avg_uv = tf2::getField(sim, "ens_avg(ux*uy)");
+    auto &avg_uw = tf2::getField(sim, "ens_avg(ux*uz)");
+    auto &avg_vv = tf2::getField(sim, "ens_avg(uy*uy)");
+    auto &avg_vw = tf2::getField(sim, "ens_avg(uy*uz)");
+    auto &avg_ww = tf2::getField(sim, "ens_avg(uz*uz)");
+
+    tf2::Field R_uu(sim, 1, "Nodes");
+    tf2::Field R_uv(sim, 1, "Nodes");
+    tf2::Field R_uw(sim, 1, "Nodes");
+    tf2::Field R_vv(sim, 1, "Nodes");
+    tf2::Field R_vw(sim, 1, "Nodes");
+    tf2::Field R_ww(sim, 1, "Nodes");
+
+    tf2::oper_fmadd(avg_u, avg_u, R_uu, 1.0, 0.0);
+    tf2::oper_axpy(avg_uu, R_uu, 1.0, -1.0);
+
+    tf2::oper_fmadd(avg_u, avg_v, R_uv, 1.0, 0.0);
+    tf2::oper_axpy(avg_uv, R_uv, 1.0, -1.0);
+
+    tf2::oper_fmadd(avg_u, avg_w, R_uw, 1.0, 0.0);
+    tf2::oper_axpy(avg_uw, R_uw, 1.0, -1.0);
+
+    tf2::oper_fmadd(avg_v, avg_v, R_vv, 1.0, 0.0);
+    tf2::oper_axpy(avg_vv, R_vv, 1.0, -1.0);
+
+    tf2::oper_fmadd(avg_v, avg_w, R_vw, 1.0, 0.0);
+    tf2::oper_axpy(avg_vw, R_vw, 1.0, -1.0);
+
+    tf2::oper_fmadd(avg_w, avg_w, R_ww, 1.0, 0.0);
+    tf2::oper_axpy(avg_ww, R_ww, 1.0, -1.0);
+
+    const auto &facesYPos = calcFacesYPos(sim);
+    auto NY = facesYPos.size()-1;
+    std::vector<double> avgLR_uu(NY);
+    std::vector<double> avgLR_uv(NY);
+    std::vector<double> avgLR_uw(NY);
+    std::vector<double> avgLR_vv(NY);
+    std::vector<double> avgLR_vw(NY);
+    std::vector<double> avgLR_ww(NY);
+    std::vector<double> volumesL(NY);
+    std::vector<double> yL(NY, -DBL_MAX);
+    const auto &mesh = tf2::getSMesh(sim);
+    std::vector<double> buffer_uu(tf2::getNumNodes(mesh));
+    std::vector<double> buffer_uv(tf2::getNumNodes(mesh));
+    std::vector<double> buffer_uw(tf2::getNumNodes(mesh));
+    std::vector<double> buffer_vv(tf2::getNumNodes(mesh));
+    std::vector<double> buffer_vw(tf2::getNumNodes(mesh));
+    std::vector<double> buffer_ww(tf2::getNumNodes(mesh));
+    tf2::oper_getData(R_uu, buffer_uu.data());
+    tf2::oper_getData(R_uv, buffer_uv.data());
+    tf2::oper_getData(R_uw, buffer_uw.data());
+    tf2::oper_getData(R_vv, buffer_vv.data());
+    tf2::oper_getData(R_vw, buffer_vw.data());
+    tf2::oper_getData(R_ww, buffer_ww.data());
+    auto const cells = tf2::getNumCells(mesh);
+    for (int c = 0; c<cells; ++c)
+    {
+        const auto &C = tf2::getCellIJKFromId(mesh,c);
+        double cy = tf2::calcCellCentroid(mesh,C)[1];
+        double vol = tf2::calcCellVolume(mesh,C);
+        uint64_t idp = bsearchVectorE(facesYPos, cy, snap);
+        avgLR_uu[idp] += vol*buffer_uu[c];
+        avgLR_uv[idp] += vol*buffer_uv[c];
+        avgLR_uw[idp] += vol*buffer_uw[c];
+        avgLR_vv[idp] += vol*buffer_vv[c];
+        avgLR_vw[idp] += vol*buffer_vw[c];
+        avgLR_ww[idp] += vol*buffer_ww[c];
+        volumesL[idp] += vol;
+        yL[idp] = cy;
+    }
+
+    auto &GY = tf2::getMatrix(sim, "GradY_NF");
+    tf2::Field dudy(sim, 1, "Faces");
+    tf2::oper_prod(GY, avg_u, dudy);
+    auto &fab = tf2::getField(sim, "avgBottom");
+    double bottomArea = sim.IOParamD["bottomArea"];
+    double sdudy = fabs(tf2::oper_dot(fab, dudy)[0])/bottomArea;
+
+    const auto &outP = sim.IOParamS["outPrefix"];
+    const auto &avgR_uu = allReduce(avgLR_uu, MPI_SUM);
+    const auto &avgR_uv = allReduce(avgLR_uv, MPI_SUM);
+    const auto &avgR_uw = allReduce(avgLR_uw, MPI_SUM);
+    const auto &avgR_vv = allReduce(avgLR_vv, MPI_SUM);
+    const auto &avgR_vw = allReduce(avgLR_vw, MPI_SUM);
+    const auto &avgR_ww = allReduce(avgLR_ww, MPI_SUM);
+    const auto &volumes = allReduce(volumesL, MPI_SUM);
+    const auto Re = sim.IOParamD["Re_tau"];
+    const auto fct = sim.IOParamD["RKfct"];
+    const auto method = sim.IOParamS["RKmethod"];
+    const auto &y = allReduce(yL, MPI_MAX);
+    {
+    std::ofstream out("results/reyStress_Re"+std::to_string(Re)+"_fct"+std::to_string(fct)+"_faces_"+method+".dat");
+    TF_uAssert(out.is_open(), "could not open filename for output");
+    out<<"#y y+ R_uu R_vv R_ww R_uv R_uw R_vw\n";
+    out<<std::scientific<<std::setprecision(8);
+    if (tf2::mpiRank() == 0)
+    {
+        for (uint32_t k = 0; k < NY; k++)
+        {
+            out<<y[k]
+               <<" "<<y[k]*sdudy
+               <<" "<<avgR_uu[k]/volumes[k]
+               <<" "<<avgR_vv[k]/volumes[k]
+               <<" "<<avgR_ww[k]/volumes[k]
+               <<" "<<avgR_uv[k]/volumes[k]
+               <<" "<<avgR_uw[k]/volumes[k]
+               <<" "<<avgR_vw[k]/volumes[k]
+               <<"\n";
+        }
+    }
+    out.close();
+    }
+}
+
+
+void apply_ensemble_average(tf2::Field &src, tf2::Field &dest)
+{
+    TF_uAssert(dest.dim == 1);
+    TF_uAssert(&tf2::getDomain(src) == &tf2::getDomain(dest));
+
+    std::vector<double> srcBuffer(tf2::getNumEntries(src));
+    std::vector<double> destBuffer(tf2::getNumEntries(dest));
+
+    tf2::oper_getData(src, srcBuffer.data());
+
+    auto srcElem = tf2::getDomain(src).localSize;
+    for (uint32_t k = 0; k < srcElem; k++)
+    {
+        double avg = 0.0;
+        for (uint32_t d = 0; d < src.dim; d++) avg += srcBuffer[k*src.dim+d];
+        destBuffer[k] = avg/TO_F64(src.dim);
+    }
+
+    tf2::oper_setData(dest, destBuffer.data());
+}
+
+void ensemble_avg(tf2::Simulation &sim)
+{
+    auto &ux = tf2::getField(sim, "avg(ux_N)");
+    auto &uy = tf2::getField(sim, "avg(uy_N)");
+    auto &uz = tf2::getField(sim, "avg(uz_N)");
+    auto &uxux = tf2::getField(sim, "avg(ux_N*ux_N)");
+    auto &uxuy = tf2::getField(sim, "avg(ux_N*uy_N)");
+    auto &uxuz = tf2::getField(sim, "avg(ux_N*uz_N)");
+    auto &uyuy = tf2::getField(sim, "avg(uy_N*uy_N)");
+    auto &uyuz = tf2::getField(sim, "avg(uy_N*uz_N)");
+    auto &uzuz = tf2::getField(sim, "avg(uz_N*uz_N)");
+
+
+
+    auto &ensAvg_ux = tf2::getOrCreateField(sim, 1, "ens_avg(ux)", "Nodes");
+    auto &ensAvg_uy = tf2::getOrCreateField(sim, 1, "ens_avg(uy)", "Nodes");
+    auto &ensAvg_uz = tf2::getOrCreateField(sim, 1, "ens_avg(uz)", "Nodes");
+    auto &ensAvg_uxux = tf2::getOrCreateField(sim, 1, "ens_avg(ux*ux)", "Nodes");
+    auto &ensAvg_uxuy = tf2::getOrCreateField(sim, 1, "ens_avg(ux*uy)", "Nodes");
+    auto &ensAvg_uxuz = tf2::getOrCreateField(sim, 1, "ens_avg(ux*uz)", "Nodes");
+    auto &ensAvg_uyuy = tf2::getOrCreateField(sim, 1, "ens_avg(uy*uy)", "Nodes");
+    auto &ensAvg_uyuz = tf2::getOrCreateField(sim, 1, "ens_avg(uy*uz)", "Nodes");
+    auto &ensAvg_uzuz = tf2::getOrCreateField(sim, 1, "ens_avg(uz*uz)", "Nodes");
+
+    apply_ensemble_average(ux, ensAvg_ux);
+    apply_ensemble_average(uy, ensAvg_uy);
+    apply_ensemble_average(uz, ensAvg_uz);
+    apply_ensemble_average(uxux, ensAvg_uxux);
+    apply_ensemble_average(uxuy, ensAvg_uxuy);
+    apply_ensemble_average(uxuz, ensAvg_uxuz);
+    apply_ensemble_average(uyuy, ensAvg_uyuy);
+    apply_ensemble_average(uyuz, ensAvg_uyuz);
+    apply_ensemble_average(uzuz, ensAvg_uzuz);
+}
+
+}
---
+++ parallel_in_time/channel_flow/specific_mods/utils.cpp
@@ -0,0 +1,300 @@
+#include "tf2/Opers.h"
+#include "tf2/Simulation.h"
+
+#include <vector>
+#include <fstream>
+
+void setup_cfl(tf2::Simulation &sim)
+{
+    double &DX = sim.IOParamD["dx"];
+    auto &omega = tf2::getField(sim, "Omega_C");
+    double v = tf2::oper_min(omega)[0];
+    DX = tf2::allReduce(pow(v, 1.0/3.0), MPI_MIN);
+    const double kinVisc = sim.IOParamD["kinVisc"];
+    double &tVisc = sim.IOParamD["tVisc"];
+    tVisc = 0.2*DX*DX/kinVisc;
+    sim.IOParamD["_TimeStep"] = tVisc;
+}
+
+TF_Func void init_props(tf2::Simulation &sim)
+{
+    double Re_tau = sim.IOParamD["Re_tau"];
+    double &kinVisc = sim.IOParamD["kinVisc"];
+    double &Ub = sim.IOParamD["Ub"];
+    double &d = sim.IOParamD["delta"];
+    double Ly = tf2::getLength(tf2::getSMesh(sim))[1];
+    sim.IOParamD["_MaxTime"] = 1e5;
+    sim.IOParamD["_RAn"] = Ub;
+    sim.IOParamD["_RAn1"] = Ub;
+
+    d = 0.5*Ly;
+    Ub = d*0.5*Re_tau;
+    kinVisc = 1.0/Re_tau;
+    auto &msx = tf2::getField(sim, "momSrcx_C");
+    double gp = sim.IOParamD["gradp"];
+    tf2::oper_setConst(msx, gp);
+
+    setup_cfl(sim);
+    tf2::info("init_props completed.\n");
+}
+
+double my_rand0(double)
+{
+    return (1.0-2.0*drand48());
+}
+
+TF_Func void randomize_u(tf2::Simulation &sim)
+{
+    srand48(tf2::mpiRank());
+    auto &uy = tf2::getField(sim, "uy_N");
+    auto &uz = tf2::getField(sim, "uz_N");
+
+    // Since we are passing 'my_rand0' as the argument to 'oper_apply', and
+    // 'my_rand0' is a function that maps a double into a double (as opposed to
+    // mapping a tf2::Vn into a tf2::Vn), it will work irrespective of how many
+    // components the field has: it will be applied for every component of
+    // every element.
+    tf2::oper_apply(uy, my_rand0);
+    tf2::oper_apply(uz, my_rand0);
+    tf2::info("randomize_u completed\n");
+}
+
+TF_Func void init_profile_ux(tf2::Simulation &sim)
+{
+    auto Ub = sim.IOParamD["Ub"];
+    auto d = sim.IOParamD["delta"];
+    auto dim = tf2::getField(sim, "ux_N").dim;
+    auto profile = [=](double, double y, double) -> tf2::Vn
+    {
+        // We want to apply the same profile for all components / simulations,
+        // so we just replicate it 'dim' times.
+        tf2::Vn result(dim, Ub*y/d*(2.0-y/d)+0.01*Ub*my_rand0(.1));
+        return result;
+    };
+    tf2::initField(sim, "ux_N", profile);
+    tf2::info("init_profile_ux completed.\n");
+
+    auto &INF = tf2::getMatrix(sim,"Interp_NF");
+
+    auto &ux = tf2::getField(sim,"ux_N");
+    auto &uy = tf2::getField(sim,"uy_N");
+    auto &uz = tf2::getField(sim,"uz_N");
+
+    auto &uxf = tf2::getField(sim,"ux_F");
+    auto &uyf = tf2::getField(sim,"uy_F");
+    auto &uzf = tf2::getField(sim,"uz_F");
+
+    tf2::oper_prod(INF,ux,uxf);
+    tf2::oper_prod(INF,uy,uyf);
+    tf2::oper_prod(INF,uz,uzf);
+}
+
+void printPhi(tf2::Simulation &sim)
+{
+  std::string Re = std::to_string(sim.IOParamD["Re_tau"]);
+  std::string Rkfct = std::to_string(sim.IOParamD["RKfct"]);
+  std::string RKmethod = sim.IOParamS["RKmethod"];
+
+  std::string filename = "results/phi_Re_tau-"+Re+"_RKfct-"+Rkfct+"_"+RKmethod+".dat";
+  std::ofstream file;
+
+  static bool first = true;
+  if (first) {
+    file.open(filename);
+    first = false;
+  }
+  else
+    file.open(filename,std::ios::app);
+  
+  if (file.is_open()){
+    file << sim.IOParamI["_Iter"] << "\t" << atan(sim.IOParamD["_EVimag"]/sim.IOParamD["_EVreal"]) << std::endl; 
+    file.close();;
+  }
+}
+
+TF_Func void init_omega(tf2::Simulation &sim)
+{
+    // For this particular application we need a field with the volume of each
+    // cell, replicated in all the components. This volume field will be used
+    // in the FSM, to scale the divergence of the predictor velocity. This is
+    // needed because we use the laplacian multiplied by the volume as the
+    // pressure equation, in order to make the matrix SPD.
+    auto dim    = tf2::getField(sim, "ux_N").dim;
+    auto &omega = tf2::getOrCreateField(sim, dim, "Omega_C", "Cells");
+    const auto &m = tf2::getSMesh(sim);
+    auto cells = tf2::getNumCells(m);
+    std::vector<double> buffer(tf2::getNumEntries(omega));
+
+    for (uint32_t c = 0; c < cells; c++)
+    {
+        const auto &C = tf2::getCellIJKFromId(m, c);
+        double v = tf2::calcCellVolume(m, C);
+        for (uint32_t d = 0; d < dim; d++)
+        {
+            buffer[c*dim+d] = v;
+        }
+    }
+    tf2::oper_setData(omega, buffer.data());
+    tf2::info("init_omega completed.\n");
+}
+
+TF_Func bool update_DT_cfl(tf2::Simulation &sim) 
+{
+	auto &ux  = tf2::getField(sim, "ux_N");
+	auto &uy  = tf2::getField(sim, "uy_N");
+	auto &uz  = tf2::getField(sim, "uz_N");
+	auto &mod = TF_getTmpField(sim, ux);
+	auto &tmp = TF_getTmpField(sim, ux);
+
+	tf2::oper_axpy(ux,mod,1.0,0.0);
+	tf2::oper_prod(ux,mod,1.0);
+	
+	tf2::oper_axpy(uy,tmp,1.0,0.0);
+	tf2::oper_prod(uy,tmp,1.0);
+	tf2::oper_axpy(tmp,mod,1.0,1.0);
+
+	tf2::oper_axpy(uz,tmp,1.0,0.0);
+	tf2::oper_prod(uz,tmp,1.0);
+	tf2::oper_axpy(tmp,mod,1.0,1.0);
+	
+	const double uref = sqrt(tf2::max(tf2::oper_max(mod)));
+	const double cfl = sim.IOParamD["cfl"];
+	const double tVisc = sim.IOParamD["tVisc"];
+	const double DX = sim.IOParamD["dx"]; 
+	sim.IOParamD["_TimeStep"] = cfl*std::min(0.35*DX/uref, tVisc);
+	return tf2::Iter_Continue;
+} 
+
+bool determineSteadyState(double max_x, double max_u, tf2::Simulation &sim)
+{
+  if(sim.IOParamI["_Iter"]%1000 == 0)
+  {
+      if(fabs(max_x-max_u)/max_u < 1e-1){
+        sim.IOParamD["_MaxTime"] = sim.IOParamD["_ElapsedTime"] + sim.IOParamD["nFT"]*4*M_PI/(0.65*sim.IOParamD["maxU"]);
+        sim.IOParamI["_TAVG_Start"] = sim.IOParamI["_Iter"];
+        return true;
+      }
+      sim.IOParamD["maxU"] = max_x;
+      return false;
+  } 
+}
+
+bool rollingSteadyState(double max_x, int window_size, tf2::Simulation &sim)
+{
+  double curr_ra = sim.IOParamD["_RAn"];
+  curr_ra += max_x;
+  if(sim.IOParamI["_Iter"]%window_size==0)
+  {
+    curr_ra /= (double) window_size;
+    if (fabs(curr_ra-sim.IOParamD["_RAn1"])/sim.IOParamD["_RAn1"] < 1e-1){
+        sim.IOParamD["_MaxTime"] = sim.IOParamD["_ElapsedTime"] + sim.IOParamD["nFT"]*4*M_PI/(0.65*sim.IOParamD["maxU"]);
+        sim.IOParamI["_TAVG_Start"] = sim.IOParamI["_Iter"];
+        return true;
+    }
+    else
+      sim.IOParamD["_RAn1"] = sim.IOParamD["_RAn"];
+  }
+  return false;
+}
+
+TF_Func bool monitor(tf2::Simulation &sim)
+{
+    static bool first = true;
+    static bool steady_state = false;
+    const uint32_t ndim = tf2::getField(sim,"ux_N").dim;
+    if (ndim>1){
+    if (first)
+    {
+        tf2::info("# "
+                  " ite wclock "
+                  " ts time "
+                  " resSolver "
+                  "\n");
+        first = false;
+    }
+
+    if (sim.IOParamI["_Iter"]%10 == 0)
+    {
+    static auto s = tf2::getSolver(sim, "Pressure_Solver");
+    tf2::info("%d %.8e %.5e %.5e %.5e\n",
+              sim.IOParamI["_Iter"], runTime(sim),
+              sim.IOParamD["_TimeStep"], sim.IOParamD["_ElapsedTime"],
+              tf2::oper_solve_residual(s));
+    }
+    }
+    else
+    {
+    if (first)
+    {
+        tf2::info("# "
+                  " ite wclock "
+                  " ts time "
+                  " resSolver "
+		  " max(ux) "
+                  "\n");
+        first = false;
+    }
+    double max_x;
+    if (sim.IOParamI["_Iter"]%10 == 0)
+    {
+    printPhi(sim);
+    auto &ux = tf2::getField(sim, "ux_N");
+    max_x = std::max(fabs(tf2::oper_max(ux)[0]), fabs(tf2::oper_min(ux)[0]));
+    static auto s = tf2::getSolver(sim, "Pressure_Solver");
+    tf2::info("%d %.8e %.5e %.5e %.5e %.5e\n",
+              sim.IOParamI["_Iter"], runTime(sim),
+              sim.IOParamD["_TimeStep"], sim.IOParamD["_ElapsedTime"],
+              tf2::oper_solve_residual(s),max_x);
+    }
+    
+    if(!steady_state){ steady_state = rollingSteadyState(max_x,100,sim);
+      if(steady_state) tf2::info("Steady state achieved\n");
+    }
+    }
+    return tf2::Iter_Continue;
+}
+
+TF_Func void setupManualBocos(tf2::Simulation &sim)
+{
+auto &u = tf2::getField(sim, "ux_N");
+auto cells = tf2::getDomainSize(sim, "Cells");
+std::vector<double> buffer(tf2::getNumEntries(u), 1.0);
+for (uint32_t it = cells*u.dim; it < buffer.size(); it++)
+{
+    buffer[it] = 0.0;
+}
+auto &cellMask = tf2::getOrCreateField(sim, "bnd", u);
+tf2::oper_setData(cellMask, buffer.data());
+}
+
+TF_Func bool applyManualBocos(tf2::Simulation &sim)
+{
+    auto &bnd = tf2::getField(sim,"bnd");
+	
+    auto &ux = tf2::getField(sim,"ux_N");
+    auto &uy = tf2::getField(sim,"uy_N");
+    auto &uz = tf2::getField(sim,"uz_N");
+
+    tf2::oper_prod(bnd,ux);
+    tf2::oper_prod(bnd,uy);
+    tf2::oper_prod(bnd,uz);
+
+    return tf2::Iter_Continue;
+}
+
+TF_Func void calc_div_u(tf2::Simulation &sim)
+{
+    auto &DX = tf2::getMatrix(sim, "DivX_FC");
+    auto &DY = tf2::getMatrix(sim, "DivY_FC");
+    auto &DZ = tf2::getMatrix(sim, "DivZ_FC");
+
+    auto &ux = tf2::getField(sim, "ux_F");
+    auto &uy = tf2::getField(sim, "uy_F");
+    auto &uz = tf2::getField(sim, "uz_F");
+
+    auto &div = tf2::getOrCreateField(sim, "div(u)", DX, ux);
+    tf2::oper_prod(DX, ux, div, 1.0, 0.0);
+    tf2::oper_prod(DY, uy, div, 1.0, 1.0);
+    tf2::oper_prod(DZ, uz, div, 1.0, 1.0);
+}
+
